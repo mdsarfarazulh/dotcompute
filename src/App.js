705f185143a021017563e975c0950c79688647d3
mdsarfarazulh <mdsarfarazulh@gmail.com>
2022-03-03 17:48:23 +0530

    Using material ui package for designing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import { Container } from 'react-bootstrap';
+import { Typography } from '@mui/material';
 import './App.css';
 
 function App() {
   return (
-    <Container className="App">
-      <h1>Dotcompute</h1>
-      <h2>Coming soon...</h2>
-    </Container>
+    <div className="App">
+      <Typography variant='h1' component='div' gutterBottom>Dot Compute</Typography> 
+      <Typography variant='h2' component='div' gutterBottom>Coming soon...</Typography>
+    </div>
   );
 }
 
